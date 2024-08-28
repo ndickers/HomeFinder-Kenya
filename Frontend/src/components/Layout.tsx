b41ac14434259807bd126efed7e1d44ad108f24b
jelimo-charity <charityjelimo893@gmail.com>
2024-08-28 20:39:01 +0300

    homefinder kenya - landing page setup

---
+++ Frontend/src/components/Layout.tsx
@@ -0,0 +1,19 @@
+import Footer from "./Footer";
+import Header from "./Header";
+
+ 
+interface LayoutProps {
+  children: React.ReactNode;  
+}
+ 
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <>
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </>
+  );
+};
+ 
+export default Layout;
