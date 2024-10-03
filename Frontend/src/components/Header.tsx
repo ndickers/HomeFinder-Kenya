7c7b7a86803c71e1cb0a1b5a197135e28685c539
bryondickers <bryondickers@gmail.com>
2024-10-03 13:55:59 +0300

    implement responsive navbar

--- Frontend/src/components/Header.tsx
+++ Frontend/src/components/Header.tsx
@@ -1,9 +1,25 @@
+import { useEffect, useState } from "react";
 import logo from "../assets/logo2.png";
-
+import burgerIcon from "../assets/menu-icon.png";
+import cancelIcon from "../assets/close-icon.png";
 const Header = () => {
+  const [screenWidth, setScreenWidth] = useState(window.screen.width);
+  const [showMenuIcon, setShowMenuIcon] = useState(false);
+
+  function handleScreenSize() {
+    setScreenWidth(window.screen.width);
+  }
+  useEffect(() => {
+    window.addEventListener("resize", handleScreenSize);
+    return () => {
+      window.removeEventListener("resize", handleScreenSize);
+    };
+  }, []);
+
+  console.log(screenWidth);
   return (
-    <div className="navbar bg-base-100">
-      <div className="flex-1">
+    <header className="navbar fixed flex flex-col md:flex-row bg-base-100 top-0 left-0">
+      <div className="md:flex-1 flex justify-between w-[100%]">
         {/* Logo on the left (as an image) */}
         <img
           src={logo}
@@ -11,36 +27,79 @@ const Header = () => {
           alt="Logo"
           className="h-20"
         />
+        {screenWidth <= 768 && showMenuIcon ? (
+          <button
+            onClick={() => {
+              setShowMenuIcon(false);
+            }}
+            className="md:hidden mr-4"
+          >
+            <img src={cancelIcon} alt="menu icon" />
+          </button>
+        ) : (
+          <button
+            onClick={() => {
+              setShowMenuIcon(true);
+            }}
+            className="md:hidden mr-4"
+          >
+            <img src={burgerIcon} alt="menu icon" />
+          </button>
+        )}
       </div>
-      <div className="flex-none">
-        {/* Search bar in the middle */}
-        <div className="form-control mx-4">
-          <input
-            type="text"
-            placeholder="Search properties"
-            className="input input-bordered"
-          />
-        </div>
-      </div>
-      <div className="flex-none">
-        {/* Link items on the right */}
-        <ul className="menu menu-horizontal px-1">
-          <li>
-            <a>Home</a>
-          </li>
-          <li>
-            <a>About Us</a>
-          </li>
-          <li>
-            <a>Properties</a>
-          </li>
-          <li>
-            <a>Contact Us</a>
-          </li>
-        </ul>
-      </div>
-    </div>
+
+      {showMenuIcon && (
+        <nav className="md:flex-none  flex justify-between w-[100%] md:w-auto">
+          {/* Link items on the right */}
+          <ul className="menu md:menu-horizontal menu-dropdown-show md:px-1">
+            <li>
+              <a>Home</a>
+            </li>
+            <li>
+              <a>About Us</a>
+            </li>
+            <li>
+              <a>Properties</a>
+            </li>
+            <li>
+              <a>Contact Us</a>
+            </li>
+          </ul>
+        </nav>
+      )}
+      {!showMenuIcon && screenWidth > 768 && (
+        <nav className="md:flex-none  flex justify-between w-[100%] md:w-auto">
+          {/* Link items on the right */}
+          <ul className="menu md:menu-horizontal menu-dropdown-show md:px-1">
+            <li>
+              <a>Home</a>
+            </li>
+            <li>
+              <a>About Us</a>
+            </li>
+            <li>
+              <a>Properties</a>
+            </li>
+            <li>
+              <a>Contact Us</a>
+            </li>
+          </ul>
+        </nav>
+      )}
+    </header>
   );
 };
 
 export default Header;
+
+{
+  /* <div className="flex-none">
+<div className="form-control mx-4">
+  <input
+    type="text"
+    placeholder="Search properties"
+    className="input input-bordered"
+  />
+</div>
+</div> */
+}
--- Frontend/src/components/Hero.tsx
+++ Frontend/src/components/Hero.tsx
@@ -1,25 +1,25 @@
-import hero from '../assets/hero2.jpg';
+import hero from "../assets/hero2.jpg";
 
 const Hero = () => {
   return (
-    <div 
-      className="hero min-h-screen bg-cover bg-center" 
+    <div
+      className="hero min-h-screen bg-cover bg-center"
       style={{ backgroundImage: `url(${hero})` }} // Apply the background image correctly
     >
       <div className="hero-overlay bg-opacity-60"></div>
-      <div className="hero-content text-center text-neutral-content">
+      <div className=" text-center text-neutral-content">
         <div className="max-w-lg">
-          <h1 className="mb-5 text-5xl font-bold">Discover Your Next Home in Kenya</h1>
+          <h1 className="mb-5 text-5xl font-bold">
+            Discover Your Next Home in Kenya
+          </h1>
           <p className="mb-5 text-xl">
             Search thousands of rental properties in your preferred location.
           </p>
-          <button className="btn btn-primary">
-            Start Your Search
-          </button>
+          <button className="btn btn-primary">Start Your Search</button>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default Hero;
